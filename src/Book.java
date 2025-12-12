4318562f941ace1af5997f95594ad8cd33640ff5
Weldys Andrade do Carmo <weldyscarmo27@gmail.com>
2025-12-12 09:12:12 -0300

    Class Book

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    String name;
+    Author author;
+    String synopsis;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+}
